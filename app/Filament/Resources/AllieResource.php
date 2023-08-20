d22c1a2beb1de4a2f2a594f4345881c18d45e8fc
Marzio Perez <marzioperez@gmail.com>
2023-08-20 04:17:08 -0500

    Ajuste biografía aliado

--- app/Filament/Resources/AllieResource.php
+++ app/Filament/Resources/AllieResource.php
@@ -39,7 +39,7 @@ class AllieResource extends Resource
                         Forms\Components\TextInput::make('instagram_url')->label('URL Instagram')->columnSpan(['sm' => 1, 'xl' => 3]),
                         Forms\Components\TextInput::make('email_url')->label('E-mail')->columnSpan(['sm' => 1, 'xl' => 3]),
                         Forms\Components\TextInput::make('whatsapp_url')->label('WhatsApp')->columnSpan(['sm' => 1, 'xl' => 3]),
-
+                        Forms\Components\RichEditor::make('biography')->required()->toolbarButtons(['bold', 'italic', 'link', 'undo', 'strike', 'h2', 'h3', 'orderList'])->label('Biografía')->columnSpanFull()->columnSpan(['sm' => 1, 'xl' => 12]),
                         Forms\Components\Textarea::make('iframe')->label('Iframe video')->columnSpanFull()->columnSpan(['sm' => 1, 'xl' => 12]),
                         Forms\Components\SpatieMediaLibraryFileUpload::make('featured_image')->label('Logo')->collection('main')->columnSpanFull()->columnSpan(['sm' => 1, 'xl' => 12]),
                     ])
